eee8a962ff670ed714e17d05e33711fcc0cde9ff
goncalogomes <goncalogomes@WIN-GESFLHLR4EE>
2019-11-18 17:48:56 +0000

    Adicionar arquivos de projeto.

---
+++ primos/primos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primos
+{
+    class primos
+    {
+        private Double valor;
+
+        public string recebe
+        {
+            set
+            {
+                valor = Convert.ToDouble(value);
+            }
+        }
+
+
+        public string envia
+        {
+            get
+            {
+                bool primo = true;
+                for (int i = 2; i < valor; i++) ;
+                {
+                    if (valor % 1 == 0) primo = false;
+                }
+                if (primo) return "O numero " + valor + " é Primo";
+                else return "O numero " + valor + "nao é Primo"
+            }
+        }
+    }   
+}
